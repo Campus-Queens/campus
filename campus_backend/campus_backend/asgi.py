d30000df615f1e3f46909d22e995e018c4e7169c
Liam Harper-McCabe <liamharpermccabe@Liams-MacBook-Air.local>
2025-04-10 16:50:25 -0400

    Fixing messaging

--- campus_backend/campus_backend/asgi.py
+++ campus_backend/campus_backend/asgi.py
@@ -6,18 +6,15 @@ django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat_messages.routing import websocket_urlpatterns
-from appuser.middleware import TokenAuthMiddleware
+from appuser.middleware import TokenAuthMiddleware  # <- your custom JWT middleware
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            TokenAuthMiddleware(
-                URLRouter(websocket_urlpatterns)
-            )
+        TokenAuthMiddleware(
+            URLRouter(websocket_urlpatterns)
         )
     ),
 })
